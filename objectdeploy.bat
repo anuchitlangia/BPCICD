30b360dc421d69a3faedb771b7ef73f66c8d3d0f
anuchitlangia <anuchitlangia.26@gmail.com>
2023-04-27 06:42:31 +0200

    Update objectdeploy.bat

--- objectdeploy.bat
+++ objectdeploy.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /dbconname "LocalDB Connection" /importrelease "C:\GIT\BPCICD\release 1\GPT-3.bprelease" /overwrite /user admin ashaDC@462
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /dbconname LocalDB Connection /importrelease "C:\GIT\BPCICD\release 1\GPT-3.bprelease" /overwrite /user admin ashaDC@462
